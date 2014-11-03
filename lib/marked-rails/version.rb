545ea4dc62ac73bb9cacec0eaca67aca30e139ca
Ross Cooperman <rcooperman@movableink.com>
2014-11-03 16:34:19 -0500

    Bump marked version

--- lib/marked-rails/version.rb
+++ lib/marked-rails/version.rb
@@ -1,6 +1,6 @@
 module Marked
   module Rails
-    MARKED_VERSION = "0.2.10"
-    VERSION        = "#{MARKED_VERSION}.2"
+    MARKED_VERSION = "0.3.2"
+    VERSION        = "#{MARKED_VERSION}.0"
   end
 end
